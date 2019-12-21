33f90b930bd51b89634b26b7cdb2f68fe89e8df3
Xecus <imkkai@hotmail.com>
2019-12-22 00:16:06 +0800

    使用类来实现

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,83 @@
 import axios, { AxiosResponse } from 'axios';
 import rawFs, { promises as fs, WriteStream } from 'fs';
 import { Stream } from 'stream';
+import { EventEmitter } from 'events';
 
 const videoApiURL: string = 'https://api.twitter.com/1.1/videos/tweet/config/',
       videoPlayerURL: string = 'https://twitter.com/i/videos/tweet/';
 
-export async function getBearerToken(twitter_id: string): Promise<string>{
-    let playerResponse: AxiosResponse<string> = await axios.get(`${videoPlayerURL}${twitter_id}`);
-    let jsMatchRes = playerResponse.data.match(/src="(.*js)"/);
-    if(jsMatchRes != null){
-        console.log(`jsUrl :${jsMatchRes[1]}`);
-        let jsUrl: string = jsMatchRes[1],
-            jsResponse: AxiosResponse<string> = await axios.get(jsUrl),
-            jsData: string = jsResponse.data;
-        await fs.writeFile('./jsdata.js', jsData);
-        let tokenMatchRes = jsData.match(/Bearer ([a-zA-Z0-9%-]+)/);
-        if(tokenMatchRes != null){
-            console.log(`token: ${tokenMatchRes[1]}`);
-            return tokenMatchRes[1];
-        }
-        else{
-            console.error('can`t find token!');
-        }
-    }
-    return '';
-}
+export default class Downloader extends EventEmitter{
+    private debugFlag: boolean;
 
-export async function getPlayList(twitter_id: string, token: string): Promise<string>{
-    let url = `${videoApiURL}${twitter_id}.json`,
-        videoConfigResponse: AxiosResponse = await axios.get(url, {
-            headers: {
-                "Authorization": `Bearer ${token}`
+    constructor(debugFlag?: boolean){
+        super();
+        this.debugFlag = debugFlag || false;
+    }
+
+    private async getBearerToken(twitter_id: string): Promise<string | null>{
+        let playerResponse: AxiosResponse<string> = await axios.get(`${videoPlayerURL}${twitter_id}`);
+        let jsMatchRes = playerResponse.data.match(/src="(.*js)"/);
+
+        if(jsMatchRes != null){
+            this.debugFlag && console.log(`jsUrl :${jsMatchRes[1]}`);
+
+            let jsUrl: string = jsMatchRes[1],
+                jsResponse: AxiosResponse<string> = await axios.get(jsUrl),
+                jsData: string = jsResponse.data,
+                tokenMatchRes = jsData.match(/Bearer ([a-zA-Z0-9%-]+)/);
+
+            if(tokenMatchRes != null){
+                this.debugFlag && console.log(`token: ${tokenMatchRes[1]}`);
+                return tokenMatchRes[1];
             }
-        }),
-        videoConfig = videoConfigResponse.data;
-    if(videoConfig && videoConfig.track){
-        return videoConfig.track.playbackUrl;
+            else{
+                this.debugFlag && console.error('can`t find token!');
+            }
+        }
+        return null;
     }
-    return '';
-}
 
-export async function downLoadFile(url: string, path: string): Promise<void>{
-    let response: AxiosResponse = await axios.get(url, {
-        responseType: "stream"
-    }),
-    downloadStream: Stream = response.data;
-    let writeStream: WriteStream = rawFs.createWriteStream(path);
-    downloadStream.pipe(writeStream);
+    private async getPlayUrl(twitter_id: string, token: string): Promise<string | null>{
+        let url = `${videoApiURL}${twitter_id}.json`,
+            videoConfigResponse: AxiosResponse = await axios.get(url, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            }),
+            videoConfig = videoConfigResponse.data;
+
+        if(videoConfig && videoConfig.track){
+            this.debugFlag && console.log(`play url: ${videoConfig.track.playbackUrl}`);
+            return videoConfig.track.playbackUrl;
+        }
+
+        return null;
+    }
+
+    private async downLoadFile(url: string, path: string): Promise<void>{
+        let response: AxiosResponse = await axios.get(url, {
+            responseType: "stream"
+        }),
+        downloadStream: Stream = response.data;
+
+        let writeStream: WriteStream = rawFs.createWriteStream(path);
+        
+        downloadStream.pipe(writeStream);
+    }
+
+    public async download(twitterId: string, path: string): Promise<void>{
+        let token: string | null = await this.getBearerToken(twitterId);
+        if(!token){
+            throw new Error('get Bearer Token Fail!');   
+        }
+
+        let url: string | null = await this.getPlayUrl(twitterId, token);
+        if(!url){
+            throw new Error('get video URL Fail!');
+        }
+
+        if(url.endsWith(".mp4")){
+            await this.downLoadFile(url, `${path}${twitterId}.mp4`);
+        }
+    }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,11 @@
-import { getPlayList, downLoadFile } from './index';
+import Downloader from './index';
 
 let targetId: string = '1207958103641034758';
-let token: string = "AAAAAAAAAAAAAAAAAAAAAIK1zgAAAAAA2tUWuhGZ2JceoId5GwYWU5GspY4%3DUq7gzFoCZs1QfwGoVdvSac3IniczZEYXIcDyumCauIXpcAPorE";
+
+let path: string = "/home/kaiser/website/";
 
 async function main(): Promise<void>{
-    let str: string = await getPlayList(targetId, token);
-    if(str.endsWith(".mp4")){
-        await downLoadFile(str, `/home/kaiser/website/static/${targetId}.mp4`);
-    }
+    let downloader = new Downloader(true);
+    await downloader.download(targetId, path);
 }
 main();
